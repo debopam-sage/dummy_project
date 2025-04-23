9089198ec09565ba55c080870b25fb2f02b83361
Debopam Roy <debopam@metasage.ai>
2025-04-23 12:37:52 +0530

    Final Updates

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <h1>Vite + React + TypeScript + Supabase</h1>
+      <h1>Vite + React + TypeScript</h1>
       <Counter initialValue={0} />
     </div>
   )
